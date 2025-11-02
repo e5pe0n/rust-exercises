d99c635b60dbd1ae96e37f32b260cc7249417b52
e5pe0n <1izard.synchr0fi5h@gmail.com>
2025-11-02 19:11:10 +0900

    ch06: iterators

--- ch06_iterators/src/main.rs
+++ ch06_iterators/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let f = File::open("numbers.txt")?;
+    let reader = BufReader::new(f);
+
+    let sum_of_odd_numbers: i32 = reader
+        .lines()
+        .filter_map(|line| line.ok())
+        .filter_map(|s| s.parse::<i32>().ok())
+        .filter(|n| n % 2 != 0)
+        .sum();
+
+    assert_eq!(sum_of_odd_numbers, 31);
+    Ok(())
 }
