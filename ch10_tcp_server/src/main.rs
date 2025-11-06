0576de56b1b5f212a83b7b840dcd683b59d9d5d8
e5pe0n <1izard.synchr0fi5h@gmail.com>
2025-11-06 19:15:51 +0900

    ch10-01: log

--- ch10_tcp_server/src/main.rs
+++ ch10_tcp_server/src/main.rs
@@ -1,17 +1,21 @@
 use std::{
     io::{self, BufRead, Write},
     net::{TcpListener, TcpStream},
+    sync::{Arc, Mutex},
     thread,
 };
 
-fn handle_client(stream: TcpStream) -> Result<(), io::Error> {
+fn handle_client(stream: TcpStream, log_mutex: Arc<Mutex<Vec<usize>>>) -> Result<(), io::Error> {
     let buf_reader = io::BufReader::new(&stream);
     let mut buf_writer = io::BufWriter::new(&stream);
     for line in buf_reader.lines() {
         if let Ok(line) = line {
+            let mut log = log_mutex.lock().unwrap();
+            log.push(line.len());
             let resp = "> ".to_string() + &line + "\n";
             buf_writer.write(resp.as_bytes())?;
             buf_writer.flush()?;
+            println!("{:?}", log);
         }
     }
 
@@ -20,15 +24,19 @@ fn handle_client(stream: TcpStream) -> Result<(), io::Error> {
 
 fn main() -> Result<(), io::Error> {
     let listener = TcpListener::bind("127.0.0.1:7878")?;
+    let log_mutex: Arc<Mutex<Vec<usize>>> = Arc::new(Mutex::new(vec![]));
 
     for stream in listener.incoming() {
-        if let Ok(stream) = stream {
-            thread::spawn(|| {
-                _ = handle_client(stream);
-            });
-        } else {
-            eprintln!("Bad connection");
-            continue;
+        {
+            let log_mutex = Arc::clone(&log_mutex);
+            if let Ok(stream) = stream {
+                thread::spawn(move || {
+                    _ = handle_client(stream, log_mutex);
+                });
+            } else {
+                eprintln!("Bad connection");
+                continue;
+            }
         }
     }
     Ok(())
