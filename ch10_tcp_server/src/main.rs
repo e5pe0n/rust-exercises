6485b36488a584103e6f02d12cd19e7849081944
e5pe0n <1izard.synchr0fi5h@gmail.com>
2025-11-06 18:48:42 +0900

    ch10: impl echo server

--- ch10_tcp_server/src/main.rs
+++ ch10_tcp_server/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    io::{self, BufRead, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn handle_client(stream: TcpStream) -> Result<(), io::Error> {
+    let buf_reader = io::BufReader::new(&stream);
+    let mut buf_writer = io::BufWriter::new(&stream);
+    for line in buf_reader.lines() {
+        if let Ok(line) = line {
+            let resp = "> ".to_string() + &line + "\n";
+            buf_writer.write(resp.as_bytes())?;
+            buf_writer.flush()?;
+        }
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<(), io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:7878")?;
+
+    for stream in listener.incoming() {
+        handle_client(stream?)?;
+    }
+    Ok(())
 }
