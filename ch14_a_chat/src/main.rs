b5eeaaa4c9109efc001affbdbcee84c766a5d364
e5pe0n <1izard.synchr0fi5h@gmail.com>
2025-11-08 16:08:36 +0900

    ch14: async chat

---
+++ ch14_a_chat/src/main.rs
@@ -0,0 +1,147 @@
+use std::collections::hash_map::{Entry, HashMap};
+
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::{TcpListener, TcpStream, ToSocketAddrs, tcp::OwnedWriteHalf},
+    sync::mpsc,
+    task,
+};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+type Sender<T> = mpsc::UnboundedSender<T>;
+type Receiver<T> = mpsc::UnboundedReceiver<T>;
+
+#[tokio::main]
+pub(crate) async fn main() -> Result<()> {
+    accept_loop("127.0.0.1:8080").await
+}
+
+async fn accept_loop(addr: impl ToSocketAddrs) -> Result<()> {
+    let listener = TcpListener::bind(addr).await?;
+
+    let (broker_sender, broker_receiver) = mpsc::unbounded_channel();
+    let _broker = task::spawn(broker_loop(broker_receiver));
+
+    while let Ok((stream, _socket_addr)) = listener.accept().await {
+        println!("accepting from: {}", stream.peer_addr()?);
+        spawn_and_log_error(connection_loop(broker_sender.clone(), stream));
+    }
+
+    Ok(())
+}
+
+async fn connection_loop(broker: Sender<Event>, stream: TcpStream) -> Result<()> {
+    let (reader, writer) = stream.into_split();
+    let reader = BufReader::new(reader);
+    let mut lines = reader.lines();
+
+    let name = match lines.next_line().await {
+        Ok(Some(line)) => line,
+        Ok(None) => return Err("peer disconnected immediately")?,
+        Err(e) => return Err(Box::new(e)),
+    };
+
+    println!("user {} connected", name);
+
+    broker
+        .send(Event::NewPeer {
+            name: name.clone(),
+            stream: writer,
+        })
+        .unwrap();
+
+    loop {
+        if let Some(line) = lines.next_line().await? {
+            let (dest, msg) = match line.find(':') {
+                None => continue,
+                Some(idx) => (&line[..idx], line[idx + 1..].trim()),
+            };
+            let dest = dest
+                .split(',')
+                .map(|name| name.trim().to_string())
+                .collect::<Vec<_>>();
+            let msg = msg.to_string();
+
+            broker
+                .send(Event::Message {
+                    from: name.clone(),
+                    to: dest,
+                    msg,
+                })
+                .unwrap();
+        } else {
+            break;
+        }
+    }
+
+    Ok(())
+}
+
+async fn connection_write_loop(
+    messages: &mut Receiver<String>,
+    stream: &mut OwnedWriteHalf,
+) -> Result<()> {
+    loop {
+        let msg = messages.recv().await;
+        match msg {
+            Some(msg) => stream.write_all(msg.as_bytes()).await?,
+            None => break,
+        }
+    }
+    Ok(())
+}
+
+#[derive(Debug)]
+enum Event {
+    NewPeer {
+        name: String,
+        stream: OwnedWriteHalf,
+    },
+    Message {
+        from: String,
+        to: Vec<String>,
+        msg: String,
+    },
+}
+
+async fn broker_loop(mut events: Receiver<Event>) {
+    let mut peers: HashMap<String, Sender<String>> = HashMap::new();
+
+    loop {
+        let event = match events.recv().await {
+            Some(event) => event,
+            None => break,
+        };
+
+        match event {
+            Event::Message { from, to, msg } => {
+                for addr in to {
+                    if let Some(peer) = peers.get_mut(&addr) {
+                        let msg = format!("from {from}: {msg}\n");
+                        peer.send(msg).unwrap();
+                    }
+                }
+            }
+            Event::NewPeer { name, mut stream } => match peers.entry(name.clone()) {
+                Entry::Occupied(..) => (),
+                Entry::Vacant(entry) => {
+                    let (client_sender, mut client_receiver) = mpsc::unbounded_channel();
+                    entry.insert(client_sender);
+                    spawn_and_log_error(async move {
+                        connection_write_loop(&mut client_receiver, &mut stream).await
+                    });
+                }
+            },
+        }
+    }
+}
+
+fn spawn_and_log_error<F: Future<Output = Result<()>> + Send + 'static>(
+    fut: F,
+) -> task::JoinHandle<()> {
+    task::spawn(async move {
+        if let Err(e) = fut.await {
+            eprintln!("{}", e);
+        }
+    })
+}
